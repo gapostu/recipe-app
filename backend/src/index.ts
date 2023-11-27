addd67eb165a53e4a643074557ecc01b488caad8
Gabriel Apostu <gabiapostu@yahoo.com>
2023-11-27 18:56:17 +0200

    Finished App

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import * as RecipeAPI from "./recipe-api";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
+const prismaClient = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
@@ -19,6 +20,51 @@ app.get("/api/recipes/:recipeId/summary", async (req, res) => {
   const recipeId = req.params.recipeId;
   const results = await RecipeAPI.getRecipeSummary(recipeId);
   return res.json(results);
+});
+
+app.post("/api/recipes/favourite", async (req, res) => {
+  const recipeId = req.body.recipeId;
+
+  try {
+    const favouriteRecipe = await prismaClient.favouriteRecipes.create({
+      data: {
+        recipeId: recipeId,
+      },
+    });
+    return res.status(201).json(favouriteRecipe);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Oops, something went wrong" });
+  }
+});
+
+app.get("/api/recipes/favourite", async (req, res) => {
+  try {
+    const recipes = await prismaClient.favouriteRecipes.findMany();
+    const recipeIds = recipes.map((recipe) => recipe.recipeId.toString());
+
+    const favourites = await RecipeAPI.getFavouriteRecipesByIDs(recipeIds);
+
+    return res.json(favourites);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Oops, something went wrong" });
+  }
+});
+app.delete("/api/recipes/favourite", async (req, res) => {
+  const recipeId = req.body.recipeId;
+
+  try {
+    await prismaClient.favouriteRecipes.delete({
+      where: {
+        recipeId: recipeId,
+      },
+    });
+    return res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Oops, something went wrong" });
+  }
 });
 
 app.listen(5000, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as api from "./api";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
 import RecipeModal from "./components/RecipeModal";
+import { AiOutlineSearch } from "react-icons/ai";
+
+type Tabs = "search" | "favourites";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -11,7 +14,22 @@ const App = () => {
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(
     undefined
   );
+  const [selectedTab, setSelectedTab] = useState<Tabs>();
+  const [favouriteRecipes, setFavouriteRecipes] = useState<Recipe[]>([]);
+
   const pageNumber = useRef(1);
+
+  useEffect(() => {
+    const fetchFavouriteRecipes = async () => {
+      try {
+        const favouriteRecipes = await api.getFavouriteRecipes();
+        setFavouriteRecipes(favouriteRecipes.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchFavouriteRecipes();
+  }, []);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -33,36 +51,103 @@ const App = () => {
       console.log(error);
     }
   };
+  const addFavouriteRecipe = async (recipe: Recipe) => {
+    try {
+      await api.addFavouriteRecipe(recipe);
+      setFavouriteRecipes([...favouriteRecipes, recipe]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const removeFavouriteRecipe = async (recipe: Recipe) => {
+    try {
+      await api.removeFavouriteRecipe(recipe);
+      const updatedRecipes = favouriteRecipes.filter(
+        (favRecipe) => recipe.id !== favRecipe.id
+      );
+      setFavouriteRecipes(updatedRecipes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <>
-      <form onSubmit={(event) => handleSearchSubmit(event)}>
-        <input
-          type="text"
-          required
-          placeholder="Enter a search term"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      {recipes.map((recipe) => {
-        return (
-          <RecipeCard
-            recipe={recipe}
-            onClick={() => setSelectedRecipe(recipe)}
-          />
-        );
-      })}
-      <button className="view-more-button" onClick={handleViewMoreClick}>
-        View more
-      </button>
+    <div className="app_container">
+      <div className="header">
+        <img src="/hero-image.jpg" alt="" />
+        <div className="title">My recipe App</div>
+      </div>
+      <div className="tabs">
+        <h1
+          className={selectedTab === "search" ? "tab-active" : ""}
+          onClick={() => setSelectedTab("search")}
+        >
+          Recipe search
+        </h1>
+        <h1
+          className={selectedTab === "favourites" ? "tab-active" : ""}
+          onClick={() => setSelectedTab("favourites")}
+        >
+          Favourites
+        </h1>
+      </div>
+      {selectedTab === "search" && (
+        <>
+          <form onSubmit={(event) => handleSearchSubmit(event)}>
+            <input
+              type="text"
+              required
+              placeholder="Enter a search term"
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button type="submit">
+              <AiOutlineSearch size="40" />
+            </button>
+          </form>
+          <div className="recipe-grid">
+            {recipes.map((recipe) => {
+              const isFavourite = favouriteRecipes.some(
+                (favRecipe) => recipe.id === favRecipe.id
+              );
+              return (
+                <RecipeCard
+                  recipe={recipe}
+                  onClick={() => setSelectedRecipe(recipe)}
+                  onFavouriteButtonClick={
+                    isFavourite ? removeFavouriteRecipe : addFavouriteRecipe
+                  }
+                  isFavourite={isFavourite}
+                />
+              );
+            })}
+          </div>
+
+          <button className="view-more-button" onClick={handleViewMoreClick}>
+            View more
+          </button>
+        </>
+      )}
+      {selectedTab === "favourites" && (
+        <div className="recipe-grid">
+          {favouriteRecipes.map((recipe) => (
+            <RecipeCard
+              recipe={recipe}
+              onClick={() => setSelectedRecipe(recipe)}
+              onFavouriteButtonClick={removeFavouriteRecipe}
+              isFavourite={true}
+            />
+          ))}
+        </div>
+      )}
+
       {selectedRecipe ? (
         <RecipeModal
           recipeId={selectedRecipe.id.toString()}
           onClose={() => setSelectedRecipe(undefined)}
         />
       ) : null}
-    </>
+    </div>
   );
 };
 export default App;
